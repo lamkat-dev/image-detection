f6558da6370e88cf9ba8bec03ef92bda242948e7
lamkat-dev <Katherine.Lamb1@outlook.com>
2023-07-06 11:07:28 -0400

    code successfully produces message to local kafka cluster!

--- kafka_local.py
+++ kafka_local.py
@@ -1,8 +1,9 @@
-from confluent_kafka import Producer
 import logging
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka import Producer
 
 
-def receipt(logger, err, msg):
+def receipt(err, msg):
     if err is not None:
         print('Error: {}'.format(err))
     else:
@@ -21,5 +22,21 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
+    # Create admin client
+    admin_client = AdminClient({"bootstrap.servers": "localhost:9092"})
+
+    # Create Topic list, append new topic
+    topic_list = []
+    topic_list.append(NewTopic("test", 1, 1))
+    # Publish topic list to admin client
+    admin_client.create_topics(topic_list)
+
     # Create kafka Producer p
     p = Producer({'bootstrap.servers':'localhost:9092'})
+
+    message = "this is a test message"
+
+    p.poll(1)
+    p.produce("test", message.encode('utf-8'), callback=receipt)
+    p.flush()
+
